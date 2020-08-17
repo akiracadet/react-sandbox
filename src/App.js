aea6bc4de8fe8d3275505bd9d64deba62212b43a
Mavis Vermillion <mavis.vermillion.ichi@gmail.com>
2020-08-17 20:18:21 +0800

    First React App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Components/Person';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Person name="Mavis" age="400" />
+      <Person name="Lucy" age="17" />
+      <Person name="Juvia" age="17" />
     </div>
   );
 }
